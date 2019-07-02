e88873326e3a77074fabcefed50452987be83834
Rohit <rohitpathak987@gmail.com>
2019-07-02 16:07:42 +0100

    Touch Input Script and UI

---
+++ ProjectDrew/Assets/C# Scripts/Touch_Input.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Touch_Input : MonoBehaviour {
+
+    [SerializeField]
+    private Text Message;
+
+    private void OnMouseDown()
+    {
+        Message.text = "Got Touched";
+    }
+}
