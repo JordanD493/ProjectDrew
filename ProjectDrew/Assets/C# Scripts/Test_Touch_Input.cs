8b4752664bd884bb23b4e2cfad8bb425900cbf3a
Rohit <rohitpathak987@gmail.com>
2019-07-02 15:43:07 +0100

    added a new touch input script and a test UI

---
+++ ProjectDrew/Assets/C# Scripts/Test_Touch_Input.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test_Touch_Input : MonoBehaviour {
+
+    [SerializeField]
+    private Text Message;
+
+    private void OnMouseDown()
+    {
+        Message.text = "Touched the Screen ";
+    }
+}
