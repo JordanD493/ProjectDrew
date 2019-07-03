8e02a76621e5a7330e3a35fe38d6c9eea4957312
- <->
2019-07-03 14:49:01 +0100

    Added changes to touch input script

--- ProjectDrew/Assets/C# Scripts/Touch_Input.cs
+++ ProjectDrew/Assets/C# Scripts/Touch_Input.cs
@@ -15,25 +15,24 @@ public class Touch_Input : MonoBehaviour {
 
     private void Start()
     {
+        rb = GetComponent<Rigidbody>();
 #if UNITY_EDITOR
         Message.text = "On Windows";
 #endif
 
 #if UNITY_IOS
         Message.text = "On IOS";
-
 #endif
     }
 
 #if UNITY_IOS
-
     private void OnMouseDown()
     {
         Message.text = "Got Touched";
 
         rb.AddForce(new Vector3(0, 0, 1) * Acceleration);
-    }
 
+    }
 #endif
     //private void OnMouseDrag()
     //{
