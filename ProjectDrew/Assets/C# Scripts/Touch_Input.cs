af846c80d85b7ad9ecf6caa212da972625838e36
Rohit <rohitpathak987@gmail.com>
2019-07-03 14:15:41 +0100

    Checking IOS input

--- ProjectDrew/Assets/C# Scripts/Touch_Input.cs
+++ ProjectDrew/Assets/C# Scripts/Touch_Input.cs
@@ -15,14 +15,30 @@ public class Touch_Input : MonoBehaviour {
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+#if UNITY_EDITOR
+        Message.text = "On Windows";
+#endif
+
+#if UNITY_IOS
+        Message.text = "On IOS";
+#endif
     }
 
-    private void OnMouseDown()
-    {
-        Message.text = "Got Touched";
+    //private void OnMouseDown()
+    //{
+    //    Message.text = "Got Touched";
 
-        rb.AddForce(new Vector3(0, 0, 1) * Acceleration);
+    //    rb.AddForce(new Vector3(0, 0, 1) * Acceleration);
 
-    }
+    //}
+
+    //private void OnMouseDrag()
+    //{
+    //    rb.transform.position = new Vector3(1, 1, 1);
+    //}
+
+    //private void OnMouseOver()
+    //{
+    //    Message.text = "HoverOver";
+    //}
 }
