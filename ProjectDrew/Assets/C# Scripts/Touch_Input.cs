0e9fef27d074ba3e20bea4c9c65ba819d96eedcd
Rohit <rohitpathak987@gmail.com>
2019-07-03 11:14:34 +0100

    added cube and testing it's movement on Ipad

--- ProjectDrew/Assets/C# Scripts/Touch_Input.cs
+++ ProjectDrew/Assets/C# Scripts/Touch_Input.cs
@@ -8,8 +8,21 @@ public class Touch_Input : MonoBehaviour {
     [SerializeField]
     private Text Message;
 
+    Rigidbody rb;
+
+    [SerializeField]
+    private float Acceleration;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     private void OnMouseDown()
     {
         Message.text = "Got Touched";
+
+        rb.AddForce(new Vector3(0, 0, 1) * Acceleration);
+
     }
 }
