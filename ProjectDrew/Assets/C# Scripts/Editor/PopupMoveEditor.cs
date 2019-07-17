7c807e7c662aa5ce918683878f81fe18897cf30d
Michele Busi <1702835@uad.ac.uk>
2019-07-17 11:16:47 +0100

    Quality of life changes for tabMovement
    you can now snap to either limit using a button in the inspector

---
+++ ProjectDrew/Assets/C# Scripts/Editor/PopupMoveEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PopupMove))]
+public class PopupMoveEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        PopupMove popupMove = (PopupMove)target;
+
+        if (GUILayout.Button("Snap to begin limit"))
+        {
+            //popupMove
+        }
+
+        if (GUILayout.Button("Snap to end limit"))
+        {
+            //popupMove
+        }
+    }
+}
