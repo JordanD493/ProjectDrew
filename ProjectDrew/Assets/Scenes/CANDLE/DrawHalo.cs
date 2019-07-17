ae6f671421bf43dc2fe1d00fd0c8cb6357296c8d
Artyom87 <47386007+Artyom87@users.noreply.github.com>
2019-07-17 16:42:08 +0100

    no message

---
+++ ProjectDrew/Assets/Scenes/CANDLE/DrawHalo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawHalo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent(Halo).enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
