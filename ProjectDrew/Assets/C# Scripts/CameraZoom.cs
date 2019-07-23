6a411ae5f9ba5c5820bf475acadbfedfc5425bfc
Rohit <rohitpathak987@gmail.com>
2019-07-23 15:38:13 +0100

    added camera zoom and goal material change

---
+++ ProjectDrew/Assets/C# Scripts/CameraZoom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{    
+    [SerializeField]
+    private float zoomValueZ;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        cameraZoom();
+	}
+
+    void cameraZoom()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            
+            Camera.main.transform.position = new Vector3(10.88f, 11.55f, Camera.main.transform.position.z - zoomValueZ * Time.deltaTime);
+                                             
+        }
+    }
+}
