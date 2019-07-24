ece4393584546f3648f2e2d4a0842112d28a6898
Michele Busi <1702835@uad.ac.uk>
2019-07-24 15:18:19 +0100

    Implemented tabs support for multiple snap points

--- ProjectDrew/Assets/C# Scripts/Tab Scripts/Tabs/TabMovement.cs
+++ ProjectDrew/Assets/C# Scripts/Tab Scripts/Tabs/TabMovement.cs
@@ -16,8 +16,10 @@ public class TabMovement : MonoBehaviour
     [SerializeField] private float slideSpeed = 1;
     [Tooltip("What percentage of the way between the two limits is the tab? I.e. if the tab is at the same position as beginLimit, insert 0. If same position as endLimit, insert 1. Halfway, insert 0.5. I might try to calculate this in code in the future, but for now you'll have to do it. :)")]
     [SerializeField] private float initialPosition = 0;
-    [SerializeField] private bool snapOnRelease = true;
-    [ConditionalHide("snappingOn", true, false)]
+    //[SerializeField] private bool snapOnRelease = true;
+    [ConditionalHide("snapOnRelease", true, false)]
+    [SerializeField] private List<float> snapPoints;
+    [ConditionalHide("snapOnRelease", true, false)]
     [SerializeField] private float snapSpeed = 1;
     #endregion
     // --------------------------------------------------------------------------------------------------------------------------------------- INSPECTOR INTERFACE END
@@ -43,10 +45,28 @@ public class TabMovement : MonoBehaviour
 
     protected void OnSelectionRelease(object source, EventArgs args)
     {
-        if (snapOnRelease)
+        if (snapPoints.Count > 0)
         {
-            StartCoroutine(InterpolateToClosestSnapPoint());
+            StartCoroutine(InterpolateToSnapPoint(FindClosestSnapPoint()));
         }
+    }
+
+    private float FindClosestSnapPoint()
+    {
+        float closestSnapPoint = 0;
+        float closestSnapPointDistance = 1;
+
+        foreach (float snapPoint in snapPoints)
+        {
+            float distance = Mathf.Abs(snapPoint - TabMovementPercentage);
+            if (distance < closestSnapPointDistance)
+            {
+                closestSnapPointDistance = distance;
+                closestSnapPoint = snapPoint;
+            }
+
+        }
+        return closestSnapPoint;
     }
 
     protected virtual void OnTabMove()
@@ -68,34 +88,31 @@ public class TabMovement : MonoBehaviour
         OnTabMove();
     }
 
-    IEnumerator InterpolateToClosestSnapPoint()
+
+
+    IEnumerator InterpolateToSnapPoint(float closestSnapPoint)
     {
-        float closestSnapPoint = 0;
-        if (TabMovementPercentage > 0.5f)
+        if (TabMovementPercentage > closestSnapPoint)
         {
-            closestSnapPoint = 1;
-            while (closestSnapPoint > TabMovementPercentage)
+            while (TabMovementPercentage > closestSnapPoint)
             {
-                TabMovementPercentage = Mathf.Clamp(TabMovementPercentage += snapSpeed * Time.deltaTime, 0, 1);
+                TabMovementPercentage = TabMovementPercentage -= snapSpeed * Time.deltaTime;
                 UpdateTabTransform();
                 yield return null;
             }
-            TabMovementPercentage = 1;
-            OnSnapReached();
-            yield return null;
         }
         else
         {
-            while (closestSnapPoint < TabMovementPercentage)
+            while (TabMovementPercentage < closestSnapPoint)
             {
-                TabMovementPercentage -= snapSpeed * Time.deltaTime;
+                TabMovementPercentage += snapSpeed * Time.deltaTime;
                 UpdateTabTransform();
                 yield return null;
             }
-            TabMovementPercentage = 0;
-            OnSnapReached();
-            yield return null;
         }
+        TabMovementPercentage = closestSnapPoint;
+        OnSnapReached();
+        yield return null;
     }
 
     // ------------------------------------------------- EDITOR
