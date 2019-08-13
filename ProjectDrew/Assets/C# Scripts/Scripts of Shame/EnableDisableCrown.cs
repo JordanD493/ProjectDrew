30068bc9363036626741e738c99738616e47ddbd
Elfchao <32882808+Elfchao@users.noreply.github.com>
2019-08-13 15:52:00 +0100

    Added Libor's dust shader Reduced the range and intensity of the candle lights to stop them flickering Added crown asset Applied new Level 1 atlas Michele made a script to get the crown to work Adjusted the camera to a better angle and copied it on the 2nd level Added rotating tab asset and made it a prefab Put the new pope asset in

---
+++ ProjectDrew/Assets/C# Scripts/Scripts of Shame/EnableDisableCrown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnableDisableCrown : MonoBehaviour {
+
+    [SerializeField] private TabMovement thisControlsMe;
+    [SerializeField, Range(0, 1)] private float activeWhenTabAt;
+    [SerializeField] bool crownEnabled = false;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        thisControlsMe.SnapReached += OnSnapReached;
+    }
+
+    private void OnSnapReached(object sender, EventArgs e)
+    {
+        if (thisControlsMe.TabMovementPercentage == activeWhenTabAt)
+        {
+            crownEnabled = true;
+        }
+        else
+        {
+            crownEnabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = crownEnabled;
+    } 
+
+        
+}
