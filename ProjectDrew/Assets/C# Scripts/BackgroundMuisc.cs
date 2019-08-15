bbbe2d1225a8860c3be55165e217a3d064a700d5
DrBattleBear <1800712@uad.ac.uk>
2019-08-15 11:00:52 +0100

    Added Background Music and a Background Music Script

---
+++ ProjectDrew/Assets/C# Scripts/BackgroundMuisc.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMuisc : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private static BackgroundMuisc instance = null;
+    public static BackgroundMuisc Instance
+    {
+        get { return instance; }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
